5434a508e47a61b6b336089389aeb59d1bdf3464
supunbasura <sbasura27@gmail.com>
2023-11-22 02:26:28 +0530

    with test SVG

--- Dashbord/src/components/Regions.js
+++ Dashbord/src/components/Regions.js
@@ -1,31 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { TbCircuitSwitchClosed, TbCircuitSwitchOpen } from 'react-icons/tb'; 
-import '../Styling/Regions.css';
-import Navbar from "./Navbar"
+import SwitchOn from '../SVG/Switch_on.svg';
+import SwitchOff from '../SVG/Switch_Off.svg';
 
 function Regions() {
-    const [books, setBooks] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [buttonStatus, setButtonStatus] = useState("off");
-    const [isToggling, setIsToggling] = useState(false); // Add this state to track if a toggle is in progress
-       
+    const [isSwitchedOn, setIsSwitchedOn] = useState("off");
+    const [isToggling, setIsToggling] = useState(false);
+
     const handleToggle = async () => {
-        const newStatus = buttonStatus === "on" ? "off" : "on";
+        const newStatus = isSwitchedOn === "on" ? "off" : "on";
         const confirmationMessage = newStatus === "on" 
             ? "Are you sure you want to turn on?" 
             : "Are you sure you want to turn off?";
-        
-        // Show confirmation dialog
+
         const isConfirmed = window.confirm(confirmationMessage);
-        
-        // If user chooses "Cancel", don't proceed with the toggle
         if (!isConfirmed) {
             return;
         }
-        
-        // Optimistically set the status in UI
-        // setButtonStatus(newStatus);
-        setIsToggling(true); // Disable polling
+
+        setIsToggling(true);
 
         try {
             const response = await fetch('http://localhost:8000/send/', {
@@ -36,41 +28,24 @@ function Regions() {
                 body: JSON.stringify({ status: newStatus }),
             });
             const data = await response.json();
-            // If the server responds with a different status, update the UI
             if (data.status && data.status !== newStatus) {
-                //setButtonStatus(data.status);
+                setIsSwitchedOn(data.status);
             }
         } catch (error) {
-            // Revert the button status if there's an error
-            setButtonStatus(buttonStatus === "on" ? "on" : "off");
             alert("Failed to toggle:", error);
         } finally {
-            setIsToggling(false); // Re-enable polling
-        }
-    };
-
-    const handleFetchBooks = async () => {
-        setIsLoading(true);
-        try {
-            const response = await fetch('http://localhost:8000/api/books/');
-            const data = await response.json();
-            setBooks(data);
-        } catch (error) {
-            alert("Failed to fetch books:", error);
-        } finally {
-            setIsLoading(false);
+            setIsToggling(false);
         }
     };
 
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:8000/ws/simple_message/');
-        
+
         socket.onmessage = function(event) {
-            console.log("WebS");
             const data = JSON.parse(event.data);
+            setIsSwitchedOn(data.message.status);
             console.log(data);
             console.log(data.message.status);
-            setButtonStatus(data.message.status);
         };
 
         socket.onopen = function(event) {
@@ -82,37 +57,29 @@ function Regions() {
         };
     }, []);
 
-    // useEffect(() => {
-    //     const socket = new WebSocket('ws://localhost:8000/ws/book_data/');
-    
-    //     socket.onmessage = function(event) {
-    //         setIsLoading(true);
-    //         try{
-    //             const data = JSON.parse(event.data);
-    //             console.log("book_data received");
-    //             if (data.book_data) {
-    //                 setBooks(data.book_data);
-    //             }
-    //         } catch(error) {
-    //             alert("Failed to fetch books:", error);
-    //         } finally{
-    //             setIsLoading(false);
-    //         }
-    //     };
-        
-    // }, []);
-    
-
     return (
-        <div className="Regions">
-            <header className="Regions-header">
-                <h1>Kafka React App</h1>
-                <button onClick={handleToggle} className={buttonStatus === "on" ? "button-on" : "button-off"}>
-                    {buttonStatus === "on" ? <TbCircuitSwitchClosed size={30} /> : <TbCircuitSwitchOpen size={30} />}
-                </button>
-                <label>Status: {buttonStatus}</label>
-            </header>
+      <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+        <div>
+            {isSwitchedOn === "on" ? (
+                <img src={SwitchOn} alt="Switch On" />
+            ) : (
+                <img src={SwitchOff} alt="Switch Off" />
+            )}
         </div>
+        <div onClick={handleToggle} style={{ 
+          marginLeft: '-430px', 
+          marginBottom: '50px', 
+          opacity: 0,
+          width: '150px', 
+          height: '100px', 
+          clip: 'rect(1px, 1px, 1px, 1px)', 
+          whiteSpace: 'nowrap'
+          }}>
+            <label style={{ cursor: 'pointer' }}>
+                {isSwitchedOn === "on" ? "Switch is ON" : "Switch is OFF"}
+            </label>
+        </div>
+    </div>
     );
 }
 
